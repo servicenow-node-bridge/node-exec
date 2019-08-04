3a71f0cbc5a96077be8a9bca62abf22f1478e495
Richard Szolár <34659520+0x111@users.noreply.github.com>
2019-08-04 10:59:12 +0200

    Add base structure and files (#1)
    Add basic information and structure

---
+++ snc-node-bridge/index.js
@@ -0,0 +1,78 @@
+var restify = require('restify');
+
+function processNativeScript(req, res, next) {
+    let status = "success";
+    let result = runScriptInVM(req.body.script, true);
+    if (result.error === true) {
+        status = "error";
+    }
+    let myObj = {
+        message: status,
+        result: result.result
+    };
+    
+    res.send(myObj);
+    next();
+}
+
+function processNodeScript(req, res, next) {
+    let status = "success";
+    let result = runScriptInVM(req.body.script, false);
+    if (result.error === true) {
+        status = "error";
+    }
+    let myObj = {
+        message: "success",
+        result: result.result
+    };
+    //res.send(JSON.stringify(myObj));
+    res.send(myObj);
+    next();
+}
+
+
+
+var server = restify.createServer();
+server.use(restify.plugins.bodyParser());
+server.post('/process/native', processNativeScript);
+server.post('/process/node', processNodeScript);
+
+server.listen(8080, function() {
+    console.log('%s listening at %s', server.name, server.url);
+});
+
+
+function runScriptInVM(scriptPayload, native) {
+    let vm = null;
+    const {
+        VM,
+        NodeVM,
+        VMScript
+    } = require('vm2');
+    if (native === true) {
+        vm = new VM();
+    } else {
+        let options = {
+            require: {
+                external: true
+            }
+        };
+        vm = new NodeVM(options);
+    }
+    
+    const script = scriptPayload;
+    let result = null;
+    let error = false;
+
+    try {
+        result = vm.run(script, 'vm.js');
+    } catch (e) {
+        error = true;
+        result = e;
+    }
+
+    return {
+        error: error,
+        result: result
+    };
+}
